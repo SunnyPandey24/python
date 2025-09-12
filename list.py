3d0dcd6979512a793ca82e4ae8eec8a1ddd85677
Sunny Kumar Pandey <SUNNY_243184@saitm.ac.in>
2025-09-12 23:36:43 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+friends = ["Apple","Ornage" , 5 , 465.3 , "Sunny", "Rohan", False]
+print(friends[0])
+
+friends = ["Apple","Ornage" , 5 , 465.3 , "Sunny", "Rohan", False]
+print(friends[4])
+
+friends = ["Apple","Ornage" , 5 , 465.3 , "Sunny", "Rohan", False]
+print(friends[2])
+
+friends = ["Apple","Ornage" , 5 , 465.3 , "Sunny", "Rohan", False]
+print(friends[1])
+
+#changing function.
+
+friends = ["Apple","Ornage" , 5 , 465.3 , "Sunny", "Rohan", False]
+print(friends[0])
+friends[0] = "Grapes"
+print(friends[0])
+
+friends[4] = "Shubham"
+print(friends[4])
+print(friends[1:4])
---
+++ list_methods.py
@@ -0,0 +1,41 @@
+#append(add something at last.)
+
+friends = ["Apple","Ornage" , 5 , 465.3 ,False,  "Sunny", "Rohan"]
+print(friends)
+friends.append("Harry")
+print(friends)
+
+#insert(repalce something with actuall data)
+lists = [0,1,2,3,4,5,6,7,8,9]
+print(lists)
+lists.insert(3,11)
+print(lists)
+
+#pop(delet element)
+rooms = [1,8,7,21,15]
+print(rooms)
+rooms.pop(2)
+print(rooms)
+
+#remove(remove from  list)
+
+hostal = ["xavier", "coloumbas", "mallikarjun"]  # List banayi
+print(hostal)
+hostal.remove("coloumbas")  # Element remove kar diya
+print(hostal)
+
+
+#sort(update the list)
+rows = [0,6,4,8,9,5,1,2,3,7]
+print(rows)
+rows.sort()
+print(rows)
+
+
+#reverse(arrange the data)
+gintis = [11,34,54,32,56,78,65,23]
+print(gintis)
+lists.reverse()
+print(gintis)
+
+
